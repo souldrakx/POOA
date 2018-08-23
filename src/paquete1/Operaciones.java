bac195029eebbee3efbb4d806d8fccb1e4d49fcc
Drakx <Drakx@DESKTOP-GVO6TM1>
2018-08-22 18:00:37 -0700

    Primera actualizacion

---
+++ src/paquete1/Operaciones.java
@@ -0,0 +1,6 @@
+package paquete1;
+
+public enum Operaciones {
+		suma,resta,mult,division;
+
+}
---
+++ src/paquete1/test.java
@@ -0,0 +1,10 @@
+package paquete1;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Operaciones.mult);
+	}
+
+}
