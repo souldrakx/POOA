db488b7618d5bd653aea5decf9259b8c3674ad50
Drakx <Drakx@DESKTOP-GVO6TM1>
2018-08-22 18:19:59 -0700

    Agregar nuevo atributo

--- src/paquete1/Operaciones.java
+++ src/paquete1/Operaciones.java
@@ -1,6 +1,16 @@
 package paquete1;
 
 public enum Operaciones {
-		suma,resta,mult,division;
+		suma("+"),resta("-"),mult("*"),division("/");
+	
+	private String valor;
+	
+	Operaciones(String valor){
+		this.valor = valor;
+	}
+	
+	public String getvalor() {
+		return valor;
+	}
 
 }
--- src/paquete1/test.java
+++ src/paquete1/test.java
@@ -5,6 +5,8 @@ public class test {
 	public static void main(String[] args) {
 		
 		System.out.println(Operaciones.mult);
+		System.out.println(Operaciones.suma.getvalor());
+		System.out.println(Operaciones.suma.name());
 	}
 
 }
